8b04668f1f6c2470ff2fabfcaa8b3dc04c5d5bb5
Yeolyi <type101315@gmail.com>
2022-08-02 21:33:50 +0900

    upload.bash에 rebase 작업 추가

--- upload.bash
+++ upload.bash
@@ -1,6 +1,8 @@
 set -e # https://stackoverflow.com/questions/821396/aborting-a-shell-script-if-any-command-returns-a-non-zero-value
 
 cd ~/Developer/blog
+git fetch
+git rebase
 git submodule foreach git pull
 git add .
 git commit --amend --no-edit
