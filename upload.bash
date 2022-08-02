afaa0a5b2564eadddc36b2888c839fef88a392d8
이성열 <type101315@gmail.com>
2022-08-02 10:59:04 +0900

    upload.bash에서 blog_src 커밋 부분 제거

--- upload.bash
+++ upload.bash
@@ -1,10 +1,5 @@
 set -e # https://stackoverflow.com/questions/821396/aborting-a-shell-script-if-any-command-returns-a-non-zero-value
 
-read -r commitName
-git add .
-git commit -m "$commitName"
-git push
-
 cd ~/Developer/blog
 git submodule foreach git pull
 git add .
