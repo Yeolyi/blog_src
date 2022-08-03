a61021a6568783cedda73223515f74df47e5ed2d
Yeolyi <type101315@gmail.com>
2022-08-03 13:37:31 +0900

    모던 자바스크립트 Deep Dive ~35

---
+++ dict/language/javascript/deep-dive/chapter34/iterableNIterator.js
@@ -0,0 +1,20 @@
+const fibo = function (max) {
+    let [pre, cur] = [0, 1];
+    return {
+        [Symbol.iterator]() { return this; },
+        next() {
+            [pre, cur] = [cur, pre + cur];
+            return { value: cur, done: cur >= max }
+        }
+    }
+}
+
+let iter = fibo(10);
+for (const x of iter) {
+    console.log(x); // 1 2 3 5 8
+}
+
+iter = fibo(10);
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
---
+++ dict/language/javascript/deep-dive/chapter35/restSpread.js
@@ -0,0 +1,7 @@
+// Rest parameter
+function foo(...rest) {
+    console.log(rest);
+}
+// Spread syntax
+foo(...[1, 2, 3]);
+
