26c282391b4c5017e2d96e82bff88cda3606d7b3
이성열 <type101315@gmail.com>
2022-08-23 15:51:48 +0900

    bash에 변경된 폴더명 적용

--- upload.bash
+++ upload.bash
@@ -1,6 +1,6 @@
 set -e # https://stackoverflow.com/questions/821396/aborting-a-shell-script-if-any-command-returns-a-non-zero-value
 
-cd ~/Developer/blog
+cd ~/Developer/blog_front
 git fetch
 git rebase origin/nextJS
 
