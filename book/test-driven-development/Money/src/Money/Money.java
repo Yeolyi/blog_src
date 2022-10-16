bb54642d080dd3e06c3bb466c6347a9210899b38
Yeolyi <yeolyi1310@gmail.com>
2022-10-17 00:28:19 +0900

    테스트 주도 개발 Money 1회독

--- book/test-driven-development/Money/src/Money/Money.java
+++ book/test-driven-development/Money/src/Money/Money.java
@@ -1,10 +1,115 @@
 package Money;
 
-class Dollar {
+import java.util.Hashtable;
 
-  int amount = 10;
+interface Expression {
+  Money reduce(Bank bank, String to);
+  Expression plus(Expression addend);
+  Expression times(int multiplier);
+}
 
-  Dollar(int amount) {}
+class Money implements Expression {
 
-  void times(int multiplier) {}
+  protected int amount;
+  protected String currency;
+
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return money.amount == amount && money.currency == currency;
+  }
+
+  static Money dollar(int amount) {
+    return new Money(amount, "USD");
+  }
+
+  static Money franc(int amount) {
+    return new Money(amount, "CHF");
+  }
+
+  public Expression times(int multiplier) {
+    return new Money(amount * multiplier, currency);
+  }
+
+  String currency() {
+    return currency;
+  }
+
+  public Expression plus(Expression addend) {
+    return new Sum(this, addend);
+  }
+
+  public Money reduce(Bank bank, String to) {
+    int rate = bank.rate(currency, to);
+    return new Money(amount / rate, to);
+  }
+}
+
+class Sum implements Expression {
+
+  Expression augend;
+  Expression addend;
+
+  Sum(Expression augent, Expression addend) {
+    this.augend = augent;
+    this.addend = addend;
+  }
+
+  public Money reduce(Bank bank, String to) {
+    int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
+    return new Money(amount, to);
+  }
+
+  public Expression plus(Expression addend) {
+    return new Sum(this, addend);
+  }
+
+  public Expression times(int multiplier) {
+    return new Sum(augend.times(multiplier), addend.times(multiplier));
+  }
+}
+
+class Bank {
+
+  private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
+
+  Money reduce(Expression source, String to) {
+    return source.reduce(this, to);
+  }
+
+  int rate(String from, String to) {
+    if (from.equals(to)) {
+      return 1;
+    }
+    int rate = rates.get(new Pair(from, to));
+    return rate;
+  }
+
+  void addRate(String from, String to, int rate) {
+    rates.put(new Pair(from, to), rate);
+  }
+
+  private class Pair {
+
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+      this.from = from;
+      this.to = to;
+    }
+
+    public boolean equals(Object object) {
+      Pair pair = (Pair) object;
+      return from.equals(pair.from) && to.equals(pair.to);
+    }
+
+    public int hashCode() {
+      return 0;
+    }
+  }
 }
--- book/test-driven-development/Money/src/Money/MoneyTest.java
+++ book/test-driven-development/Money/src/Money/MoneyTest.java
@@ -1,15 +1,116 @@
 package Money;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+// testEquality에서 책에는 언급되지 않은 // Unlikely argument type for equals(): Dollar seems to be unrelated to FrancJava(1201)가 뜸.
 public class MoneyTest {
 
   @Test
   public void testMultiplication() {
-    Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    Money five = Money.dollar(5);
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
+  }
+
+  // 여기서 실패하면 곰셈 테스트도 실패함을 주의
+  @Test
+  public void testEquality() {
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(6).equals(Money.dollar(5)));
+    assertFalse(Money.franc(5).equals(Money.dollar(5)));
+  }
+
+  @Test
+  public void testFrancMultiplication() {
+    Money five = Money.franc(5);
+    assertEquals(Money.franc(10), five.times(2));
+    assertEquals(Money.franc(15), five.times(3));
+  }
+
+  @Test
+  public void testCurrency() {
+    assertEquals("USD", Money.dollar(1).currency());
+    assertEquals("CHF", Money.franc(1).currency());
+  }
+
+  @Test
+  public void testSimpleAddition() {
+    Money five = Money.dollar(5);
+    Expression sum = five.plus(five);
+    Bank bank = new Bank();
+    Money reduced = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(10), reduced);
+  }
+
+  @Test
+  public void testPlusReturnsSum() {
+    Money five = Money.dollar(5);
+    Expression result = five.plus(five);
+    Sum sum = (Sum) result;
+    assertEquals(five, sum.augend);
+    assertEquals(five, sum.addend);
+  }
+
+  @Test
+  public void testReduceSum() {
+    Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+    Bank bank = new Bank();
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(7), result);
+  }
+
+  @Test
+  public void testReduceMoney() {
+    Bank bank = new Bank();
+    Money result = bank.reduce(Money.dollar(1), "USD");
+    assertEquals(Money.dollar(1), result);
+  }
+
+  @Test
+  public void testReduceMoneyDifferentCurrency() {
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(Money.franc(2), "USD");
+    assertEquals(Money.dollar(1), result);
+  }
+
+  @Test
+  public void testIdentityRate() {
+    Bank bank = new Bank();
+    assertEquals(1, bank.rate("USD", "USD"));
+  }
+
+  @Test
+  public void testMixedAddition() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+    assertEquals(Money.dollar(10), result);
+  }
+
+  @Test
+  public void testSumPlusMoney() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(15), result);
+  }
+
+  @Test
+  public void testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(20), result);
   }
 }
