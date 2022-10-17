14b92c069e16841b0d43a6b275319ef3c232c342
Yeolyi <yeolyi1310@gmail.com>
2022-10-18 01:24:14 +0900

    테스트 주도 개발 파이썬 예시 추가

--- book/test-driven-development/xUnit/code.py
+++ book/test-driven-development/xUnit/code.py
@@ -1,15 +1,108 @@
-class WasRun:
+# 자기 참조(self-referential) 프로그래밍
+# TestCase("testMethod").run() 문자열을 전달하네??
+# 초반이 어려운데, 나중에 다시 읽어보기
+
+class TestCase:
     def __init__(self, name):
-        self.wasRun = None
+        self.name = name
 
     def run(self):
-      self.testMethod()
+        result = TestResult()
+        result.testStarted()
+        self.setUp()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
+        self.tearDown()
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        TestCase.__init__(self, name)
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log += "testMethod "
+
+    def setUp(self):
+        self.log = "setUp "
+
+    def tearDown(self):
+        self.log += "tearDown "
+
+    def testBrokenMethod(self):
+        raise Exception
 
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.failureCount = 0
+
+    def testStarted(self):
+        self.runCount += 1
+
+    def testFailed(self):
+        self.failureCount += 1
+
+    def summary(self):
+        return '%d run, %d failed' % (self.runCount, self.failureCount)
+
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+
+
+class TestCaseTest(TestCase):
+
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert (test.log == "setUp testMethod tearDown ")
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert ("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert ("1 run, 1 failed" == result.summary())
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert ("1 run, 1 failed" == result.summary())
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result = TestResult()
+        suite.run(result)
+        assert ("2 run, 1 failed" == result.summary())
+
+
+TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
+TestCaseTest("testSuite").run()
